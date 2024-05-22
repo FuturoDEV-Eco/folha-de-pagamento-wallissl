f64510c5eea611ce57f039c9de65828b3d5cada7
Wallis <wallis.sl@outlook.com>
2024-05-22 16:45:57 -0300

    Ex4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const calcularInss = require("./calculo_inss");
 const calcularIr = require("./calculo_imposto_renda");
+const salarioLiquido = require("./calculo_salario_liquido");
 const readline = require('readline');
 
 const input = readline.createInterface({
@@ -8,7 +9,7 @@ const input = readline.createInterface({
     terminal: false
 });
 
-// EX1
+// EX2
 /* input.question("Informe o valor do salário, para saber a alíquota a ser paga:", (salarioDigitado) => {
     
     let salarioBruto = parseFloat(salarioDigitado);
@@ -17,8 +18,8 @@ const input = readline.createInterface({
     console.log(`O valor do desconto é: R$${valorDesconto}`);
 }); */
 
-//EX2
-input.question("Informe o valor do salário Bruto para saber a alíquota referente ao Imposto de renda:", (salarioDigitado) => {
+//EX3
+/* input.question("Informe o valor do salário Bruto para saber a alíquota referente ao Imposto de renda:", (salarioDigitado) => {
     let salarioBruto = salarioDigitado
     let valorDesconto = calcularIr(salarioBruto);
 
@@ -28,4 +29,12 @@ input.question("Informe o valor do salário Bruto para saber a alíquota referen
         console.log(`O valor do desconto é: R$${valorDesconto.toFixed(2)}`)
     }
 })
+ */
 
+//EX4
+input.question("Informe o valor do salário Bruto para saber o saldo do seu salário liquido:", (salarioDigitado) => {
+    let salarioBruto = salarioDigitado;
+    let saldoLiquido = salarioLiquido(salarioBruto);
+
+    console.log(`O valor do seu salário liquido é: R$${saldoLiquido.toFixed(2)}`)
+})
